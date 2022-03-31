388fc10f435bd207965c596c7031b96b5b882a63
nguyentanmobile <nguyenngoctan.mobile@gmail.com>
2022-03-31 11:19:38 +0700

    initialize

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(express.static(path.join(__dirname,"/src/public/")));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.get('/content', function(req, res){
+    console.log(req.query)
+    if(req.query.contentname && req.query.contentname!==""){
+       
+        try{
+            const file = `${__dirname}/public/contents/${req.query.contentname}`;           
+            res.download(file); // Set disposition and send it.
+        }catch(e){     
+            res.send({error:"File is not exist"})
+        }finally{
+            
+        }
+    }
+  });
+
+app.listen(3000,(err)=>{
+    if(!err){
+        console.log("server is running")
+    }
+})
